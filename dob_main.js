adcd085ba0b9d3709e6fdb1d2e8ef91a8b43103d
tech849 <smarakd64@gmail.com>
2026-06-30 20:37:13 +0530

    Create dob_main.js

---
+++ dob_main.js
@@ -0,0 +1,12 @@
+<div class="cal">
+            <div style="color:black;font-size:20px;font-weight:bold;">Check your day of D.O.B</div><br>
+            <form>
+            <input type="text" placeholder="Enter Date" id="dt">
+            <input type="text" placeholder="Enter Month" id="mth">
+            <input type="text" placeholder="Enter Year" id="yr">
+            <input type="text" placeholder="Enter Century" id="cty">
+            <button type="button" onclick="calculateDay()" id="rbtn">GET RESULT</button>
+            </form>
+            <div id="throwdays"></div><br>
+            <div style="color:black;font-size:20px;font-weight:bold;">DEVELOPED BY: ARPITA DAS</div><br>
+        </div>
